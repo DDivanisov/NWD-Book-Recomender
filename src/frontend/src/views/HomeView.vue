663ac4687c7f269fbffa67d420555b9550f139fb
Darko <152779891+DDivanisov@users.noreply.github.com>
2024-09-02 20:22:39 +0200

    moded the home page

--- src/frontend/src/views/HomeView.vue
+++ src/frontend/src/views/HomeView.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="container">
-    <div class="link-left">
-      <router-link to="/likeGenres" class="link">What genres you like</router-link>
+    <div class="link-left" @click="rout('likeGenres')">
+      <span class="link">Like genres</span>
     </div>
-    <div class="link-right">
-      <router-link to="/foryou" class="link">Books that are recommended for you</router-link>
+    <div class="link-right" @click="rout('foryou')">
+      <span class="link">Recomendations</span>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HomeView',
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+
+const rout = (path) =>{
+  router.push({name: path});
 }
 </script>
 
@@ -22,25 +27,28 @@ export default {
   flex-direction: column;
   gap: 10px;
   padding: 50px;
+  height: 95vh;
 }
 
 .link-left {
   display: flex;
-  justify-content: flex-start;
+  align-self: flex-start;
   border: 1px black solid;
-  padding: 10px;
+  padding: 100px;
   margin: 10px;
-  background-color: rgb(255, 255, 255, 0.8);
+  margin-top: 2%;
+  background: url('../../public/heart_bg.png') no-repeat center/contain  rgb(255, 255, 255, 0.6);
   border-radius: 15px;
 }
 
 .link-right {
   display: flex;
-  justify-content: flex-end;
+  align-self: flex-end;
   border: 1px black solid;
   margin: 10px;
-  padding: 10px;
-  background-color: rgb(255, 255, 255, 0.8);
+  margin-top: 2%;
+  padding: 100px;
+  background: url('../../public/for-you-bg.png') no-repeat center/contain  rgb(255, 255, 255, 0.6);
   border-radius: 15px;
 }
 
@@ -48,5 +56,25 @@ export default {
   color: black;
   text-decoration: none;
   font-weight: bold;
+  font-size: 20px;
+  margin-bottom: 40px;
+}
+.link-left, .link-right {
+  width: 400px; 
+  padding: 20px;
+  height: 600px;
+  display: flex;
+  justify-content: center; 
+  align-items: center;
+}
+.link-left:hover, .link-right:hover {
+  cursor: pointer;
+  background-color: lightblue;
+  opacity: 0.7;
+
+  .link {
+    font-weight: bold;
+    color: blue;
+  }
 }
 </style>
